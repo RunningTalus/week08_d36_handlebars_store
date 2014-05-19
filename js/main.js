d3279760aab4a8c9c1f99ba3548ab4f287300c7e
runningtalus <dave@davemay.org>
2014-05-19 12:41:42 -0600

    Part I is completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 $(document).on('ready', function() {
   
-  console.log($);
-  console.log(Handlebars);
+  // console.log($);
+  // console.log(Handlebars);
+
+  var productTemplate = $('#product-template');
+  	// console.log(productTemplate);
+	var templateText = productTemplate.html();
+		// console.log(templateText);
+	var template = Handlebars.compile(templateText);
+		// console.log(template);
+	
+	for (var i=0; i<productsData.productsList.length; i++){
+  	var item = productsData.productsList[i];
+  	// console.log(item);
+  	var item1 = template(item);
+  	$('.new-products').append(item1);
+  	console.log(item1);
+  }
+
 
 });
