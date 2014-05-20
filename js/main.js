a245365cc2b87cf6ceda4ec17fd003ec20bdd8bb
runningtalus <dave@davemay.org>
2014-05-20 17:30:12 -0600

    Completed Part III section 3A.  Need to add remove buttons to wishlist

--- js/main.js
+++ js/main.js
@@ -18,18 +18,24 @@ $(document).on('ready', function() {
   	// console.log(item1);
   }
 
+
+
   var wishlistTemplate = $('#wishlist-template');
-  console.log(wishlistTemplate);
+  // console.log(wishlistTemplate);
 
   var wishlistText = wishlistTemplate.html();
-  console.log(wishlistText);
+  // console.log(wishlistText);
 
   var wishTemplateText = Handlebars.compile(wishlistText);
 
-  for (var j=0; j<productsData.productsList.length; j++){
-    var indivItem = productsData.productsList[j];
-    var indivItemJ = wishTemplateText(indivItem);
-    $('#wishlist').append(indivItemJ);
-  }
+  $('.wishlist').on('click', function(){
+    // console.log($(this));
+    var listSection = $(this).closest('.product').find('.title').html();
+    // console.log("This is listSection: " + listSection);
+    var insertThis = wishTemplateText({title:listSection});
+    // console.log("This is insertThis: " + insertThis);
+    // console.log("This is Handlebars: " + Handlebars);
+    $('#wishlist').append(insertThis);
+  });
 
 });
